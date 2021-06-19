353f2e9b1e8c84af0c43f9e4296922dd8cce4a83
TextError <savinraul1983@yahoo.com>
2021-06-19 18:01:58 +0100

    Slug Event logic

--- pages/events/[...slug].jsx
+++ pages/events/[...slug].jsx
@@ -1,5 +1,24 @@
+import { useRouter } from 'next/router';
+import data from '../../data';
 
 const Slug = () => {
+  const { query: { slug }} = useRouter();
+
+  if(!slug) return <p className='center'>Loading</p>
+
+  const [year, month] = slug;
+  const numYear = +year;
+  const numMonth = +month;
+
+  if(isNaN(numYear) || isNaN(numMonth) || numMonth < 1 || numMonth > 12) return <p>Invalid Dates</p>
+
+  const events = data.filter(ev => {
+    const date = new Date(ev.date);
+    return date.getFullYear() === numYear && date.getMonth() === numMonth - 1
+  });
+
+  if(!events || events.length === 0) return <p>No Events!</p>
+
   return (
     <div>
       
