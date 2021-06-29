41e48d12114bc78a3b04d8a0a7473cbc376b7d21
TextError <savinraul1983@yahoo.com>
2021-06-29 14:10:57 +0100

    getStaticProps from firebase

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,12 +1,20 @@
-import data from '../data';
 import EventList from '../components/common/EventList';
 
-const Home = () => {
-  const isFeaturedEvents = data.filter(ev => ev.isFeatured);
-
-  return (
-    <EventList events={isFeaturedEvents} />
-  )
-};
+const Home = ({ isFeaturedEvents }) => (
+  <EventList events={isFeaturedEvents} />
+);
 
 export default Home;
+
+export const getStaticProps = async () => {
+  const url = 'https://nextjs-max-aa84a-default-rtdb.europe-west1.firebasedatabase.app/events.json'
+  const data = await (await fetch(url)).json();
+
+  let events = [];
+  for(const key in data) events.push({ id: key, ...data[key] });
+  const isFeaturedEvents = events.filter(el => el.isFeatured);
+
+  return {
+    props: { isFeaturedEvents }
+  }
+};
