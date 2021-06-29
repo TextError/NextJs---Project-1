97013302c4ffd3dd8483de6aa92ff49463e4215b
TextError <savinraul1983@yahoo.com>
2021-06-29 14:34:24 +0100

    getStaticPaths & getStaticProps for [id] page

--- pages/events/[id].jsx
+++ pages/events/[id].jsx
@@ -1,13 +1,10 @@
-import { useRouter } from 'next/router';
-import data from '../../data';
-
 import EventSummary from '../../components/events/EventSummary';
 import EventLogistics from '../../components/events/EventLogistics';
 import EventContent from '../../components/events/EventContent';
 
-const EventId = () => {
-  const { query: { id }} = useRouter();
-  const event = data.filter(ev => ev.id === id);
+const EventId = ({ event }) => {
+  // const { query: { id }} = useRouter();
+  // const event = data.filter(ev => ev.id === id);
   const { description, title, date, location, image, alt } = event[0];
 
 
@@ -29,3 +26,31 @@ const EventId = () => {
 }
 
 export default EventId;
+
+export const getStaticPaths = async () => {
+  const url = 'https://nextjs-max-aa84a-default-rtdb.europe-west1.firebasedatabase.app/events.json'
+  const data = await (await fetch(url)).json();
+  let events = [];
+  for(const key in data) events.push({ id: key, ...data[key] });
+
+  const paths = events.map(el => ({ params: { id: el.id }}));
+
+  return { 
+    paths,
+    fallback: false
+  };
+};
+
+export const getStaticProps = async ({ params: { id }}) => {
+  const url = 'https://nextjs-max-aa84a-default-rtdb.europe-west1.firebasedatabase.app/events.json'
+  const data = await (await fetch(url)).json();
+
+  let events = [];
+  for(const key in data) events.push({ id: key, ...data[key] });
+
+  const event = events.filter(ev => ev.id === id);
+
+  return {
+    props: { event }
+  };
+};
